b92f98026f6f569a6ad4dc798ceb40b05b31a752
reijiokito <romeothecong@gmail.com>
2021-08-14 15:41:28 +0700

    update auth

--- server/index.js
+++ server/index.js
@@ -5,16 +5,18 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
+import userRouter from "./routes/user.js";
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://anhcong9x00:Iloveyou1998~@cluster0.evqkd.mongodb.net/Memories';
+const CONNECTION_URL = 'mongodb+srv://anhcong9x00:Iloveyou1998~@cluster0.evqkd.mongodb.net/Memories?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
